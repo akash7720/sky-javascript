fcfbbebf54208833c50c7091d0485b16e56e959b
akashsonune <akashsonune7720@gmail.com>
2023-10-29 01:57:26 +0530

    updates

---
+++ day-1/day-1.js
@@ -0,0 +1,53 @@
+// 1. Datatypes 
+
+// // 1. String 
+// // 2. Number 
+// // 3. Boolean 
+
+// 1. String 
+// 2. Number 
+// 3. Boolean 
+
+
+// // String 
+// String 
+
+// syntax -> '',"",``
+//  "my name i awdiz"
+// 'my age is 10 years'
+// `Hello`
+// "my name is swaraj"
+// syntax -> '',"",``
+// "my name i awdiz"
+// 'my age is 10 years'
+// `Hello`
+// "my name is swaraj"
+
+
+// console.log("my name is swaraj")
+// console.log('my name is swaraj')
+// console.log(`my name is swaraj`)
+// console.log(typeof("awdiz"))
+// console.log(typeof('awdiz'))
+// console.log(typeof(`awdiz`))
+// console.log(typeof("12345"))
+// console.log(typeof('234567'))
+
+// console.log(1+1)
+// console.log("1"+"1")
+
+
+// Number
+
+// syntax no
+
+
+console.log(2)
+console.log(23456)
+console.log(298654)
+console.log(typeof(1234567))
+console.log(typeof("298654"))
+
+
+
+
---
+++ day-2/day-2.js
@@ -0,0 +1,99 @@
+
+// 1. Pre defined function
+// 2. User created function 
+
+// function add(num1, num2){
+//     return num1 + num2
+// }
+// add(2,3)
+
+// Boolean
+
+// true , fales
+
+// console.log(true)
+// console.log(typeof (true))
+// console.log(false)
+// console.log(typeof (false))
+// console.log(typeof('false'))
+
+// undefined
+
+// varible
+
+// var
+// const 
+// let
+
+
+//  var
+// Syntax
+
+// var name="awdiz"
+// console.log(name)
+// console.log(typeof(name))
+
+// var myStudentsCount = 20;
+// console.log(myStudentsCount)
+// console.log(typeof(myStudentsCount))
+
+// var kuchbhi = true;
+// console.log(kuchbhi)
+
+// var number1 = 50;
+// var number2 = 100;
+// console.log(number1, number2)
+// console.log(number1 + number2)
+
+
+// Arthmetic operations 
+
+// + - * / %
+
+// var num1 = 10;
+// var num2 = 20;
+
+// console.log(30 % 2)
+// console.log(31 % 2)
+// console.log(123456789 % 2)
+// console.log(2 == 21)
+// console.log(40 % 2 == 0)
+
+// onsole.log(num1 + num2)
+// console.log(num1 - num2)
+// console.log(num1 * num2)
+// console.log(num1 / num2)
+
+
+// Comparison Operator  
+//  == , === 
+
+console.log(2 == 2)
+console.log("2" == 2) //  check only data
+console.log("2" === 2) // check data and datatypes
+console.log("40" == 40  )
+console.log("400" === 400)
+
+
+// logical operator 
+//  && , ||
+
+// && -all conditions must be true then it all return true otherwise fales
+
+// || - if even one condition found true then it all true , and when all 
+
+// conditions are fales then it all return fales 
+
+var userage = 124;
+
+console.log(userage > 91 && userage ==24 && userage < 25)
+console.log(userage < 19 || userage < 90 || userage < 124)
+
+
+// console.log()
+
+console.log( 2 > 1)
+console.log(4 < 2)
+
+console.log( 6 >= 6)
+console.log(10 <= 8 )
