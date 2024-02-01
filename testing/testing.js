2ae8a8eba04f96a8f9505fa6ba1cbb7bf7c35f66
akashsonune <akashsonune7720@gmail.com>
2024-02-01 11:31:12 +0530

    updates

--- testing/testing.js
+++ testing/testing.js
@@ -1,16 +1,29 @@
 
 
-var array = [1,2,3,4,5,6,7,8,9,10]
+// var array = [1,2,3,4,5,6,7,8,9,10]
 
-var target = 8; 
+// var target = 8; 
 
-for(var i = 0; i < array.length -1; i++){
+// for(var i = 0; i < array.length -1; i++){
 
-for(var j = i +1; j < array.length; j++){
+// for(var j = i +1; j < array.length; j++){
 
-    if (array[i] + array[j] == target){
-       console.log(i,j)
-    }
+//     if (array[i] + array[j] == target){
+//        console.log(i,j)
+//     }
+// }
+// }
+
+
+
+
+var age = 34
+if (20>34){
+    console.log("it is true")
+}else{
+    console.log("it is false")
 }
-}
+
+// console.log("program ended")
+
 
